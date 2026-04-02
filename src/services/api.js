fac32f1c39b5bb09d580aa14587fd67e87bfd8af
ADDALA MATHEW <addalamathew@gmail.com>
2026-04-02 09:38:46 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-backend-production-1528.up.railway.app/api",
+  baseURL: "https://ecommerce-backend-production-05de.up.railway.app/api",
 });
 
 export default API;
